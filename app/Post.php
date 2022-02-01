43f0c6ab69bd141e9619de0ba7115b78d4f45d59
shomffl <showatanabe0226@gmail.com>
2022-02-01 16:28:34 +0900

    詳細画面の作成

---
+++ app/Post.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    public function getBylimit(int $limit_count = 10){
+        return $this->orderBy("updated_at", "DESC")->limit($limit_count)->get();
+    }
+    
+    public function getPaginateByLimit(int $limit_count = 10){
+        return $this->orderBy("updated_at", "DESC")->paginate($limit_count);
+    }
+
+}
